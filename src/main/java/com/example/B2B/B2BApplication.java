07fa1fd7b5eb44bc46fa8337d1cec65ca97c1bdf
Mouad Makboul <mouadmakboul8@gmail.com>
2023-12-12 17:34:46 +0100

    file upload

--- src/main/java/com/example/B2B/B2BApplication.java
+++ src/main/java/com/example/B2B/B2BApplication.java
@@ -1,9 +1,12 @@
 package com.example.B2B;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class B2BApplication {
 
 	public static void main(String[] args) {
