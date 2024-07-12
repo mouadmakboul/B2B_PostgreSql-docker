3aef961aa8d9435a882ccac7713c5c557b22917f
mouadmakboul <mouadmakboul8@gmail.com>
2024-07-12 15:50:38 +0200

    fgehgf

--- src/main/java/com/example/B2B/Controllers/testController.java
+++ src/main/java/com/example/B2B/Controllers/testController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping
     public String returnString(){
-        return "hello worlde";
+        return "hello worlde ddd";
     }
 }
