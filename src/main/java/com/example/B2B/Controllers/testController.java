93d4a4428d9b1f773e3297e71a13dacc73d359c1
mouadmakboul <mouadmakboul8@gmail.com>
2024-07-22 23:26:17 +0200

    jpour

--- src/main/java/com/example/B2B/Controllers/testController.java
+++ src/main/java/com/example/B2B/Controllers/testController.java
@@ -8,8 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 
 public class testController {
-    @GetMapping
-    public String returnString(){
-        return "hello worlde ddd";
-    }
-}
+    @GetMapping("/")
+    public String returnHtml() {
+        String htmlContent = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <title>Page HTML</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <h1>MOUAD MAKBOUL</h1>\n" +
+                "    <h1>PR MERIEM CHERGUI</h1>\n" +
+                "    <h1>PR SAYOUTI</h1>\n" +
+                "    <h1>PR DAOUIDI</h1>\n" +
+                "    <p>Merci pour votre attention.</p>\n" +
+                "</body>\n" +
+                "</html>";
+        return htmlContent;
+}}
